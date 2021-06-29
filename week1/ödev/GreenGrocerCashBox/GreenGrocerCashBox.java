bcc8a85f9e048848bd46236f8f7c676501ea4e37
GürayKaleli <55591718+kaleliguray@users.noreply.github.com>
2021-06-29 23:44:19 +0300

    Update GreenGrocerCashBox.java

--- week1/ödev/GreenGrocerCashBox/GreenGrocerCashBox.java
+++ week1/ödev/GreenGrocerCashBox/GreenGrocerCashBox.java
@@ -13,18 +13,21 @@ public class GreenGrocerCashBox{
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-
+ 
+        // show the user the price(per kg) of the products 
         System.out.print("Fruit and per KG price :\n" 
                         +"Pear : 2,14 TL\n"
                         +"Apple : 3,67 TL\n"
                         +"Tomato : 1,11 TL\n"
                         +"Banana : 0,95 TL\n"
                         +"Aubergine : 5,00 TL\n");
-
+       
+        // define variables
         double pear, apple, tomato, banana, aubergine;
 
         double totalPrice;
 
+        // take user shopping basket
         System.out.print("How many kilos of pears do you want to buy? : ");
         pear = scanner.nextDouble();
         pear *= 2.24;
@@ -45,6 +48,7 @@ public class GreenGrocerCashBox{
         aubergine = scanner.nextDouble();
         aubergine *= 5.00;
 
+        // calculate and show order price
         totalPrice = pear + apple + tomato + banana + aubergine;
 
         System.out.println("Total Price : " + totalPrice) + "TL";
