5ab8c46004aaa12dab665d3b9ad1ee30d07a84c2
GürayKaleli <55591718+kaleliguray@users.noreply.github.com>
2021-06-29 23:38:30 +0300

    Update FibbonacciExample2.java

--- week1/ödev/Fibbonacci/FibbonacciExample2.java
+++ week1/ödev/Fibbonacci/FibbonacciExample2.java
@@ -1,9 +1,18 @@
+/**
+ * 
+ * @author Guray Kaleli,2021
+ * 
+ * @return calculation of fibonacci number and order them
+ * 
+ * */
+
 package com.week1.ödev.fibbonacci;
 
 import java.util.Scanner;
 
 public class FibbonacciExample2 {
 
+    // identify a method for fibbonacci number and take number as parameter 
     public static int fibbonacci(int number){
 
             if (number <= 1){
@@ -20,11 +29,13 @@ public class FibbonacciExample2 {
 
     public static void main(String[] args) {
 
+        // object from scanner class to get input from user
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter your fibbonacci number : ");
         int fib = scanner.nextInt();
 
+        // call fibbonacci method inside a for-loop and put "i" in method and use as a our method parameter
         for (int i = 0; i < fib; i++){
             System.out.print(fibbonacci(i) + " ");
         }
